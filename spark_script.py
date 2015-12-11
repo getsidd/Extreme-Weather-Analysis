0155ffdbb8e59e71fcf26fcad5ab4759b18ddbc2
getsidd <suryasid@buffalo.edu>
2015-12-11 05:27:13 -0500

    Update spark_script.py

--- spark_script.py
+++ spark_script.py
@@ -10,21 +10,21 @@ import csv
 import sys
 import time
 
-def parsePoint(line):
+def parsePoint(line):  #Parsing the RDD to convert it into a LabeledPoint RDD
 	values = [float(x) for x in line.split(',')]
 	return LabeledPoint(values[0], values[1:])
 
-def gen_row(row):
+def gen_row(row):  #Unpacking the tuples to lists
     s = StringIO.StringIO()
     w = csv.writer(s)
     w.writerow(row)
     return s.getvalue()
 
-def removeNull(line):
+def removeNull(line): #Removing the missing values for calculating averages
 	newData = tuple(0.0 if x==-9999 else x for x in line)
 	return newData
 
-def fillMissingValues(line):
+def fillMissingValues(line): #function that fills the missing values with the averages
  	key = line[6]+line[7][0:6]
  	newList = list(line)
  	if(newList[1]=="-9999"):
@@ -47,15 +47,15 @@ if __name__ == "__main__":
 	#conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
 	#sc = SparkContext(conf=conf)
 
-	csv_data = sc.textFile("/gpfs/courses/cse603/students/suryasid/Data_Four/*.csv",use_unicode=False)\
+	csv_data = sc.textFile("/gpfs/courses/cse603/students/suryasid/Data_Four/*.csv",use_unicode=False)\ # Load the dataset and split the data to get the required columns
 					 .map(lambda x: (x.split(",")[0],x.split(",")[1],x.split(",")[2],x.split(",")[3],x.split(",")[4],x.split(",")[5],x.split(",")[6],x.split(",")[8]))\
 					 .filter(lambda line: "EXTREME" not in line)
 	
-	csv_data_as_kv_snwd = csv_data.map(lambda x: (x[6]+x[7][0:6],float(x[2])))\
-							 .map(removeNull)\
-							 .mapValues(lambda x:(x,1))\
-							 .reduceByKey(lambda x,y:(x[0]+y[0],x[1]+y[1]))
-	csv_data_as_snwd_map = csv_data_as_kv_snwd.map(lambda (key,xy):(key,xy[0]/xy[1])).collectAsMap()
+	csv_data_as_kv_snwd = csv_data.map(lambda x: (x[6]+x[7][0:6],float(x[2])))\ # Generating the key for each station.
+							 .map(removeNull)\ # removing null values
+							 .mapValues(lambda x:(x,1))\ 
+							 .reduceByKey(lambda x,y:(x[0]+y[0],x[1]+y[1]))#calculating averages for that particular station 
+	csv_data_as_snwd_map = csv_data_as_kv_snwd.map(lambda (key,xy):(key,xy[0]/xy[1])).collectAsMap()#creating a hashmap for each feature
 	csv_data_as_kv_prcp = csv_data.map(lambda x: (x[6]+x[7][0:6],float(x[1])))\
 							 .map(removeNull)\
 							 .mapValues(lambda x:(x,1))\
@@ -77,13 +77,13 @@ if __name__ == "__main__":
 							 .reduceByKey(lambda x,y:(x[0]+y[0],x[1]+y[1]))
 	csv_data_as_tmin_map = csv_data_as_kv_tmin.map(lambda (key,xy):(key, xy[0]/xy[1])).collectAsMap()
 	csv_data.final = csv_data.map(fillMissingValues)\
-							 .map(lambda x: (x[0],x[1],x[2],x[3],x[4],x[5]))\
-							 .map(lambda (x,y,z,w,u,v): gen_row([x,y,z,w,u,v]))
-	data_parsed = csv_data.final.map(parsePoint)
-	data_parsed.cache()
-	model = SVMWithSGD.train(data_parsed, iterations=100)
-	labelsAndPreds = data_parsed.map(lambda p: (p.label, model.predict(p.features)))
-	trainErr = labelsAndPreds.filter(lambda (v, p): v != p).count() / float(data_parsed.count())
+				 .map(lambda x: (x[0],x[1],x[2],x[3],x[4],x[5]))\#Extracting just the features
+				 .map(lambda (x,y,z,w,u,v): gen_row([x,y,z,w,u,v]))# Generating list from tuple
+	data_parsed = csv_data.final.map(parsePoint) #Parsing the RDD and converting it into LabeledRDD
+	data_parsed.cache()#Caching the data
+	model = SVMWithSGD.train(data_parsed, iterations=100)#Training the data with SVM
+	labelsAndPreds = data_parsed.map(lambda p: (p.label, model.predict(p.features))) #Predicting the labels from the trained model
+	trainErr = labelsAndPreds.filter(lambda (v, p): v != p).count() / float(data_parsed.count())#Calculating the error
 	print("Training Error = " + str(trainErr))
 	end = time.time()
 	print end - start
